af01aaadc6186a5e647c8d58230ebe1ea4a34b0e
Shaohao Lin <slin232@uwo.ca>
2015-03-08 20:40:32 -0400

    removed files

--- routes/marketDepth.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by kurt on 15-3-1.
- */
-StockMarket.MarketDepthRoute = Ember.Route.extend({
-    model: function(params){
-        return this.store.find('company', params.company_id);
-    }
-});
