e013745a3ef91c0aead58266c1664a6d4b044b9a
Shaohao Lin <slin232@uwo.ca>
2015-03-08 20:39:30 -0400

    remove unnessary files

--- routes/symbol.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by kurt on 15-2-25.
- */
-StockMarket.SymbolRoute = Ember.Route.extend({
-    model: function(params) {
-        return this.store.find('company', params.company_id);
-    }
-});
