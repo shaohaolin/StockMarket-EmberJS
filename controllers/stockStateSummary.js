91910dc227c394e19e1185f84aea3bfab2cd99d9
Shaohao Lin <slin232@uwo.ca>
2015-03-09 17:15:56 -0400

    filter function implmentation

--- controllers/stockStateSummary.js
+++ controllers/stockStateSummary.js
@@ -3,10 +3,55 @@
  */
 StockMarket.StockStateSummaryController = Ember.ArrayController.extend({
 
+    theFilter: "none",
+
+    actions: {
+        filterFunction: function(choice){
+            alert(choice);
+            this.set("theFilter", choice);
+        }
+    },
+
+    checkFilterMatch: function(theFilter, netchange) {
+        if (theFilter == "increase"){
+            if (parseFloat(netchange) > 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else if (theFilter == "decrease"){
+            if (parseFloat(netchange) < 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return true;
+        }
+    },
+
+    filterCompany: (function (){
+        alert(this.get('model'));
+        return this.get('model').filter((function(_this){
+            return function(company, index, enumerable) {
+                if (_this.get("theFilter")) {
+                    return _this.checkFilterMatch(_this.get('theFilter'),company.get('netChange'));
+                }
+                else {
+                    return true;
+                }
+            };
+        })(this));
+    }).property( "theFilter"),
+
 
     //sort companies based on Company Name alphabet
     sortProperties: ['companyName:asc'],
-    sortedCompany: Ember.computed.sort('model','sortProperties')
+    sortedCompany: Ember.computed.sort('filterCompany','sortProperties')
 
     /*
     sortBuyOrderPrice : ['bidPrice:asc'],
